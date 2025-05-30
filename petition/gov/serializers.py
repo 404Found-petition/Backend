db32fe1f5a32685885291437f1e47233baaf2fdd
sky4715 <melissaliv715@gmail.com>
2025-05-30 19:16:09 +0900

    modi vote

--- petition/gov/serializers.py
+++ petition/gov/serializers.py
@@ -81,21 +81,20 @@ class PostSerializer(serializers.ModelSerializer):
     userid = serializers.SerializerMethodField()
     has_vote = serializers.SerializerMethodField()
 
-
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'created_at', 'userid', 'comments', 'has_vote']
 
     def get_userid(self, obj):
-        uid = obj.user.userid  # 사용자 ID는 항상 존재함
-        return uid[:3] + "*" * (len(uid) - 3)  # 마스킹 처리 예: tes**** 
+        uid = obj.user.userid
+        return uid[:3] + "*" * (len(uid) - 3)
 
     def get_comments(self, obj):
         comments = Comment.objects.filter(post=obj).order_by('-created_at')[:2]
         return CommentSerializer(comments, many=True).data
 
     def get_has_vote(self, obj):
-        return obj.vote_set.exists()
+        return obj.has_poll 
 
 # -------------------- 청원 관련 --------------------
 class PetitionSerializer(serializers.ModelSerializer):
